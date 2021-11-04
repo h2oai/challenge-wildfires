95c9b5eedb06189ac7ad18b0ae0a935d51726b8b
Azim Baghadiya <azim.baghadiya@h2o.ai>
2021-11-04 20:44:41 +0530

    Starter-kit wave app: rest of the src

--- wave-app/src/model.py
+++ wave-app/src/model.py
@@ -1,6 +1,53 @@
-from h2o_wave import main, app, Q, ui, on, data
+
+from h2o_wave import main, app, Q, ui
 import lightgbm as lgb
 import os
+from .ui_utils import *
+from .plot import *
+
+# FUNCTIONS FOR MODEL TAB
+
+models_dir = 'models'
+
+async def model(q:Q):
+    # GET THE AVAILABLE MODELS
+    models = list(q.app.models.keys())
+    val = q.args.models or models[0]
+
+    # SHOW MODEL SELECTION/LOAD INPUTS
+    q.page['models'] = ui.form_card(box=ui.box('data'), items=[
+            ui.combobox(name='models', label='Models', choices=models, value=val),
+            ui.buttons(justify='center', items=[
+                ui.button(name='load', label='Load', primary=True),
+            ])
+    ])
+    await q.page.save()
+
+    # LOAD SELECTED MODEL
+    await load(q, val)
+
+async def load(q:Q, val: str):
+    model = q.app.models[val]
+    # SHOW MODEL PARAMS WITH STAT CARDS
+    q.page['metrics'] = ui.form_card(box='data', items=[
+            ui.stats(justify='around', items=[
+                *[ui.stat(f'{key}', f'{val}') for (key, val) in model.params.items()]
+            ])
+        ]
+    )
+
+    # UPDATE MAP CARD TO NOTIFY BAR CHART IS BEING MADE
+    q.page['map'] = ui.form_card(box='map', items=[
+        ui.progress(label='Preparing bar chart...')
+    ])
+    await q.page.save()
+
+    # LOAD BAR CHART FOR THE FEATURE IMPORTANCE FOR THE SELECTED MODEL
+    bar_chart = f"{val.strip('.txt')}_features.html"
+    q.page['map'] = ui.form_card(box='map', items=[
+        ui.frame(content=(open(bar_chart).read()), height='600px')
+    ])
+    await q.page.save()
 
 # LOAD APP'S EXISTING MODELS STORED IN THE LOCAL FILE SYSTEM
 async def load_models(q:Q):
